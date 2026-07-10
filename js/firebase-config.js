e620969c7d96d681f12b7e99f4b6da440691f531
Naman Gupta <agraharinaman021@gmail.com>
2026-07-10 10:52:47 +0530

    Add firebase config for deployment

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgjbC0AgsA9_GG5MkPFUMwO-UEsdvGk5Q",
+  authDomain: "onenightprep-49fbc.firebaseapp.com",
+  projectId: "onenightprep-49fbc",
+  storageBucket: "onenightprep-49fbc.firebasestorage.app",
+  messagingSenderId: "27764210352",
+  appId: "1:27764210352:web:88ac24460c7e6e9d525975",
+  measurementId: "G-048189DT4N",
+};
